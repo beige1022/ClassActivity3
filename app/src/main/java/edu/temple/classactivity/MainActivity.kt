99d827e1850c5c954400cac2774ca55b2629549d
beige <markoguaman45@gmail.com>
2026-02-04 16:04:45 -0500

    Fix: Added error message for empty name input

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -9,19 +9,22 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     lateinit var displayTextView : TextView
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
 
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            if (findViewById<EditText>(R.id.nameEditText).text.isNotEmpty()) {
-                displayTextView.text =
-                    "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if (nameEditText.text.isNotEmpty()) {
+                displayTextView.text = "Hello, ${nameEditText.text}"
+                nameEditText.error = null
             } else {
-                displayTextView.text = "Please enter your name!"
+                nameEditText.error = "Please enter your name!"
+                displayTextView.text = ""
             }
         }
     }
