61df9426a715b6790df0c3ae820b1d691267930f
beige <markoguaman45@gmail.com>
2026-02-04 16:00:55 -0500

    fix: correctly implemented interface

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,28 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var displayTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        displayTextView = findViewById(R.id.displayTextView)
+
+        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
+            if (findViewById<EditText>(R.id.nameEditText).text.isNotEmpty()) {
+                displayTextView.text =
+                    "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            } else {
+                displayTextView.text = "Please enter your name!"
+            }
+        }
     }
 }
